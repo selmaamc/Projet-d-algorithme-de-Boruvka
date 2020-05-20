55d012604dc7feaef0dd49295084d586c0e0705c
Mouhcine El <mouhcineel@Mouhcines-MacBook-Air.local>
2020-05-20 13:00:50 +0000

    algo.c

--- Algorithme-Boruvka/algo.c
+++ Algorithme-Boruvka/algo.c
@@ -15,20 +15,29 @@ int main(void) {
 
 }
 void function boruvka {
-sommet tabsommets[]; //tableau contenant tous les sommets du graphe
-arete tabaretes[]; //tableau contenant toutes les arêtes du graphe
-a=sizeof(tabsommets)/sizeof(tabsommets[0]); //a est la taille du tableau tabsommets
-b=sizeof(tabaretes)/sizeof(tabaretes[0]); //b est la taille du tableau tabaretes
-for (i=0;i<a;i++) //on parcourt tous les elements du tableau, cad tous les sommets du graphe
+	sommet tabsommets[]; //tableau contenant tous les sommets du graphe
+	arete tabaretes[]; //tableau contenant toutes les arêtes du graphe
+	a=sizeof(tabsommets)/sizeof(tabsommets[0]); //a est la taille du tableau tabsommets
+	b=sizeof(tabaretes)/sizeof(tabaretes[0]); //b est la taille du tableau tabaretes
+	for (i=0;i<a;i++) //on parcourt tous les elements du tableau, cad tous les sommets du graphe
 	{
-	for (j=0;j<b;j++) //on parcourt toutes les aretes du graphes
+		for (j=0;j<b;j++) //on parcourt toutes les aretes du graphes
 		{
-		    if ((tabaretes[j].s1==tabsommets[i])||(tabaretes[j].s2==tabsommets[i])) /*si c'est un sommet de cette arête
-		    {
-
-		    }
+			if ((tabaretes[j].s1==tabsommets[i])||(tabaretes[j].s2==tabsommets[i]))
+			{
+				tabAr[k]=aretes[j];
+				k++;
+			}
 		}
-
+     		arete areteChoisie; //on choisit pour chaque sommet l'arete qui a le poids le plus bas.
+     		w=tabAr[0].poids;
+     		for (j=0;j<b;j++)
+     		{
+     	 		if (tabAr[j].poids<w)
+     	 	 	areteChoisie=tabAr[j];
+     		}
+     		tabfin[s]=areteChoisie; //on ajoute l'arête choisie au tableau tabfin
+     		s++;
+	}
 }
-}
-}
+   	
