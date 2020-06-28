1b2b7752f9aa032f6b73635716cb5a7ba7d5abb0
Mouhcine El <mouhcineel@Mouhcines-MacBook-Air.local>
2020-06-28 06:04:38 +0100

    Kruskal + Boruvk + graph de complex

--- Algorithme-Boruvka/Krusal.py
+++ Algorithme-Boruvka/Krusal.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 
 
-import time 
-from numpy.random import seed 
-from numpy.random import randint 
-import matplotlib.pyplot as plt 
+import time
+from numpy.random import seed
+from numpy.random import randint
+import matplotlib.pyplot as plt
 
 
 class Graphe:
@@ -20,7 +20,7 @@ class Graphe:
         if origine[i] == i:
             return i
         return self.chercher(origine, origine[i]) #on associe chaque 
-    
+
     def union(self, origine, rang, a, b):
         a_orig = self.chercher(origine, a)
         b_orig = self.chercher(origine, b)
@@ -32,62 +32,45 @@ class Graphe:
         #else:
          #   origine[b_orig] = a_orig
           #  rang[a_orig] += 1
-    
 
-    def boruvka(self):
+
+    def Kruskal(self):
         L = []
         origine = []
         rang = []
-        ens_min = [] #index d'arete qui a le poids minimum
-        S = self.s
-        poidsmst = 0
-        G = len(self.graph)
-        for i in range (self.s):
-            origine.append(i)
+        i = 0
+        S = 0
+        self.graph = sorted(self.graph,key=lambda item: item[2])
+        #print(self.graph)
+        origine = []
+        rang = []
+        for j in range(self.s):
+            origine.append(j)
             rang.append(0)
-            ens_min = [-1] * self.s
-        while S > 1:
-            for i in range(G):
-                src = self.graph[i][0]
-                dst = self.graph[i][1]
-                poids = self.graph[i][2]
+	while S < self.s-1 :
+                src,dst,poids = self.graph[i]
+                i += 1
                 ens1 = self.chercher(origine, src)
-                ens2 = self.chercher(origine, dst)
+                ens2 = self.chercher(origine ,dst)
                 if ens1 != ens2:
-                    if ens_min[ens1] == -1 or ens_min[ens1][2]>poids:
-                        ens_min[ens1] = [src, dst, poids]
-                    if ens_min[ens2] == -1 or ens_min[ens2][2]>poids:
-                        ens_min[ens2] = [src, dst, poids]
-            for j in range (self.s):
-                if ens_min[j] != -1:
-                    src, dst, poids = ens_min[j]
-                    ens1 = self.chercher(origine, src)
-                    ens2 = self.chercher(origine, dst)
-                    if ens1 != ens2:
-                        poidsmst += poids
-                        self.union(origine, rang, ens1, ens2)
-                        L += [src,dst]
-                        S = S - 1
-            ens_min = [-1]*self.s
-        #print L
-        #print poidsmst
+    		    L.append([src,dst,poids])
+                    self.union(origine, rang, ens1, ens2)
+                    S += 1
+    #print L
 
 
 
-##############
 
 
 
-##L = [[0, 1, 1], [0, 2,2], [1, 2, 4], [1, 3, 3], [2, 3, 5], [2, 4, 1], [3, 4, 2]]
-#g = Graphe(5)
-#g.graph = L
-#g.boruvka() 
+#L = [[[1,2,5],[2,3,6],[3,4,5],[4,1,8],[1,3,18]],[[1,2,5],[1,3,7],[2,4,8],[3,5,4],[3,6,2],[4,7,6],[4,6,4],[5,7,6]
 
-def main():  # fonction pour generer des Graphs 
+
+def main():  # fonction pour generer des Graphs
     S = 4
     L = []
     elements = list()
-    times = list() 
+    times = list()
     for i in range(1,10):
             for i in range(0,S-2):
                 L.append(list((i,i+1,randint(1,50))))
@@ -95,11 +78,11 @@ def main():  # fonction pour generer des Graphs
             L.append(list((S-2,S-1,randint(1,50))))
             #L[i].append(random.randint(0,3))
             #L[i].append(random.randint(1,50))
-         #   print(L)
+           # print(L)
             start = time.clock()
             g = Graphe(S)
             g.graph = L
-            g.boruvka()
+            g.Kruskal()
             end = time.clock()
             #print(L, "temps:", end-start)
             elements.append(S)
@@ -107,11 +90,10 @@ def main():  # fonction pour generer des Graphs
             S += 5
     plt.xlabel('List Length')
     plt.ylabel('Time Complexity')
-    plt.plot(elements, times, label ='Boruvka')
+    plt.plot(elements, times, label ='Kruskal')
     plt.grid()
     plt.legend()
     plt.show()
 
 main()
-
 
